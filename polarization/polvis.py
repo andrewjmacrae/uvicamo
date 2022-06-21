296f9629eacc851514363ab9d1d6d4eac6482238
Charlie Squires <cjsquires@users.noreply.github.com>
2022-06-21 12:36:10 -0700

    Cleaning spacing issues and tab inconsistency

--- polarization/polvis.py
+++ polarization/polvis.py
@@ -1,6 +1,7 @@
 # ps_polvis.py
 
-run_offline = True
+run_offline = True # testing (True) or measuring (False)
+do_save = False
 
 import swptools as swp
 import numpy as np
@@ -12,12 +13,13 @@ if not run_offline:
     from daqhats import mcc118, OptionFlags, HatIDs, HatError
     from daqhats_utils import select_hat_device, enum_mask_to_string, chan_list_to_mask	
 
+# applying variable names to the paths to the json files
 sim_settings_file = 'settings/simsettings.json'
 daq_settings_file = 'settings/daqsettings.json'
 swp_settings_file = 'settings/swpsettings.json'
 
-do_save = False
-# Load data from json file
+
+# load sim settings json
 if run_offline:
 	if not os.path.isfile(sim_settings_file):
 		print(f'Error: simulation file {sim_settings_file} not found.')
@@ -34,6 +36,7 @@ if run_offline:
 			sim_wp_phi = simpams['wp_phase']
 			sim_trigger_phase = simpams['sim_phase_offset']
 			sim_poltype = simpams['sim_poltype']
+			
 			if sim_poltype == 'right':
 				S_sim = np.array([1,0,0,1])
 			elif sim_poltype == 'lin':
@@ -41,6 +44,7 @@ if run_offline:
 			else:
 				S_sim = np.array([1,np.sqrt(.3),np.sqrt(.3),np.sqrt(.4)])
 
+# load daq settings json
 if not os.path.isfile(daq_settings_file):
 	print(f'Error: simulation file {sim_settings_file} not found.')
 	print('Run \'gen_default_json.py\' to generate default file first.')
@@ -56,13 +60,15 @@ else:
 		Ts = 1/scan_rate
 		tTot = Ts*Nsmp
 		t = np.linspace(0,tTot-Ts,Nsmp)
+		
 		if not run_offline:
 			channel_mask = chan_list_to_mask(channels)
 			num_channels = len(channels)
 			address = select_hat_device(HatIDs.MCC_118)
 			hat = mcc118(address)
 			options = OptionFlags.CONTINUOUS
-            
+
+# load swp settings json
 if not os.path.isfile(swp_settings_file):
 	print(f'Error: settings file {swp_settings_file} not found.')
 	print('Run \'gen_default_json.py\' to generate default file first.')
@@ -75,6 +81,7 @@ else:
 		auto_scale_y_trace = swppams['auto_scale_y_trace']
 		bg_level = swppams['bg_level']
 		data_log_file = swppams['log_data_file']
+		
 		if data_log_file != '':
 			do_save = True
 			with open(data_log_file,'w') as f:
@@ -82,144 +89,181 @@ else:
 
 # Set up plot canvas
 fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15, 5))
-# Plots
+# Define plots
 ln1, = ax1.plot([], [], lw=2)
 bar = ax2.bar([0, 1, 2, 3], [0, 0, 0, 0], align='center')
 bar2 = ax2.bar([0, 1, 2, 3], [0, 0, 0, 0], align='edge',alpha = .3)
 ln3, = ax3.plot([],[], lw=2, label = 'trace')
-#text variables to update
+# Define text variables to update
 txt1 = ax1.text(-.95,0.9,'',fontsize = 12)
 txt2 = ax1.text(-.95, 0.8, '', fontsize = 12, color = 'blue')
 txt_err = ax1.text(-1.25,-1.35,'', fontsize = 10, color = 'red')
 
-def init_animation():        
-    #graph parameters for Ellipse
-    ax1.set_xlim(-1.0, 1.0)
-    ax1.set_ylim(-1.0, 1.0)
-    ax1.set_xlabel('$E_x$')
-    ax1.set_ylabel('$E_y$')
-    ax1.set_title('Polarization Ellipse')
-    ax1.grid()
-    ax1.set_aspect('equal')
-    
-    #add unit circle around ellipse
-    t_circ = np.linspace(0,2*np.pi,1000)
-    x = [np.cos(T) for T in t_circ]
-    y = [np.sin(T) for T in t_circ]
-    ax1.plot(x,y, color = 'gray', linestyle = '--', alpha = 0.5)
+def init_animation():
+	"""
+	Sets the parameters for the 3 graphs, used as the initial function in FuncAnimation.
+	Allows setting the graph parameters without cluttering up a single function
 
-    #graph parameters for Bar graph
-    ax2.set_xlim(-0.6, 3.6)
-    ax2.set_ylim(-1.05, 1.05)
-    ax2.set_xticks([0, 1, 2, 3])
-    ax2.set_xticklabels(['S0', 'S1', 'S2', 'S3'])
-    ax2.set_xlabel('Stokes Parameter')
-    #ax2.set_ylabel('Value of Stokes Parameter')
-    ax2.set_title('Stokes Parameters')
-    
-    ax3.set_xlim(-1,1000)
-    ax3.set_ylim(0,3)
-    ax3.set_title('Trace')
-    ax3.grid()
-        
-    return ln1,bar,txt1,ln3,txt_err
-    
+	Parameters:
+		None
+
+	Returns:
+		ln1, bar, ln3: paramaterized functions
+		txt1, txt_err: accompanying text
+	"""
+	
+	#graph parameters for Ellipse
+	ax1.set_xlim(-1.0, 1.0)
+	ax1.set_ylim(-1.0, 1.0)
+	ax1.set_xlabel('$E_x$')
+	ax1.set_ylabel('$E_y$')
+	ax1.set_title('Polarization Ellipse')
+	ax1.grid()
+	ax1.set_aspect('equal')
+	
+	#add unit circle around ellipse
+	t_circ = np.linspace(0,2*np.pi,1000)
+	x = [np.cos(T) for T in t_circ]
+	y = [np.sin(T) for T in t_circ]
+	ax1.plot(x,y, color = 'gray', linestyle = '--', alpha = 0.5)
+	
+	#graph parameters for Bar graph
+	ax2.set_xlim(-0.6, 3.6)
+	ax2.set_ylim(-1.05, 1.05)
+	ax2.set_xticks([0, 1, 2, 3])
+	ax2.set_xticklabels(['S0', 'S1', 'S2', 'S3'])
+	ax2.set_xlabel('Stokes Parameter')
+	#ax2.set_ylabel('Value of Stokes Parameter')
+	ax2.set_title('Stokes Parameters')
+	
+	ax3.set_xlim(-1,1000)
+	ax3.set_ylim(0,3)
+	ax3.set_title('Trace')
+	ax3.grid()
+	
+	return ln1,bar,txt1,ln3,txt_err
+
 def animate_fun(idx):
-    global phs,t, S_sim
-    estr = 'warnings: '
-    if run_offline:
-        DP = sim_DOP
-        Phi = float(idx/18.)
-        w = 2*np.pi*5100/60        
-        if np.mod(int(idx/50),3) == 0:
-            S_sim = 3*np.array([1,DP*np.cos(Phi)/np.sqrt(2),DP*np.sin(Phi)/np.sqrt(2),DP/np.sqrt(2)])
-            estr = 'ellip-pol. '+estr
-        elif np.mod(int(idx/50),3) == 1:            
-            S_sim = 3*np.array([1,DP*np.cos(Phi),DP*np.sin(Phi),0])
-            estr = 'lin-pol. '+estr
-        else:
-            S_sim = 3*np.array([1,0,0,DP])
-            estr = 'circ-pol. '+estr
-        y1 = swp.sim_pol_data(S_sim,w,t,ns_level=sim_ns_level,sig_level = sim_siglevel,digitize_mV=sim_digitize, v_bias = sim_bg_level,dphi=sim_wp_phi,ofst = sim_trigger_phase)
-        # y1 = swp.sim_pol_data(S_sim,w,t,ofst = sim_trigger_phase)
-        y2 = 5*(np.mod(w*t,2*np.pi) < np.pi/12)
-    else:
-        hat.a_in_scan_start(channel_mask, samples_per_channel, scan_rate, options)
-        read_result = hat.a_in_scan_read(samples_per_channel, timeout)
-        y1 = np.array(read_result.data[::2]) - bg_level
-        y2 = read_result.data[1::2]
-        hat.a_in_scan_stop()
-        hat.a_in_scan_cleanup()
+	"""
+	Defines the function FuncAnimation will animate
+	
+	Parameters:
+		idx: int
+		current frame in the animation
 
-    trigz = swp.extract_triggers(y2)
+		# note: this parameter is not to be called independantly, its inclusion 
+		is because FuncAnimation requires such a parameter to work
 
-# I'm rewriting this part of the algorithm, if only because I'm too stoopid to git it. - AM
-    Nchunks = len(trigz)-1
+	Returns:
+		ln1, bar, ln3: 
+		graphs to be animated
 
-    Nroll = 0 # Holds rolling average of pts per chunk (PPC). Used for accuracy warning.
-    # For each chunk we calculate the 0,2w, and 4w comonents, averaged over all chunks
-    # get_stokes_from_chunk(cnk,wp_ret = np.pi/2,phs_ofst = 0,verbose = False):
+		txt1: 
 
-    S = np.zeros(4)
+	"""
+	
+	global phs,t, S_sim
+	estr = 'warnings: '
+	
+	if run_offline:
+		DP = sim_DOP
+		Phi = float(idx/18.)
+		w = 2*np.pi*5100/60
+		
+		if np.mod(int(idx/50),3) == 0:
+			S_sim = 3*np.array([1,DP*np.cos(Phi)/np.sqrt(2),DP*np.sin(Phi)/np.sqrt(2),DP/np.sqrt(2)])
+			estr = 'ellip-pol. '+estr
+			
+		elif np.mod(int(idx/50),3) == 1:            
+			S_sim = 3*np.array([1,DP*np.cos(Phi),DP*np.sin(Phi),0])
+			estr = 'lin-pol. '+estr
+			
+		else:
+			S_sim = 3*np.array([1,0,0,DP])
+			estr = 'circ-pol. '+estr
+			
+		y1 = swp.sim_pol_data(S_sim,w,t,ns_level=sim_ns_level,sig_level = sim_siglevel,digitize_mV=sim_digitize, v_bias = sim_bg_level,dphi=sim_wp_phi,ofst = sim_trigger_phase)
+		# y1 = swp.sim_pol_data(S_sim,w,t,ofst = sim_trigger_phase)
+		y2 = 5*(np.mod(w*t,2*np.pi) < np.pi/12)
+		
+	else:
+		hat.a_in_scan_start(channel_mask, samples_per_channel, scan_rate, options)
+		read_result = hat.a_in_scan_read(samples_per_channel, timeout)
+		y1 = np.array(read_result.data[::2]) - bg_level
+		y2 = read_result.data[1::2]
+		hat.a_in_scan_stop()
+		hat.a_in_scan_cleanup()
 
-    for k in range(Nchunks):
-        chunk = np.array(y1[trigz[k]:trigz[k+1]])
-        #get_stokes_from_chunk(cnk,wp_ret = np.pi/2,phs_ofst = 0,verbose = False):
-        S += swp.get_stokes_from_chunk(chunk,wp_ret = wp_phi,phs_ofst = trigger_phase,verbose = False)
-        Nroll = (Nroll*k + len(chunk))/(k+1) # Update (PPC)
-    #computing Stokes Parameters from Fourier Shenanigans (see analysis document)
-    S/=Nchunks
-    S /= S[0]
-    
-    DOP = np.sqrt(S[1]**2 + S[2]**2 + S[3]**2)
-    if do_save:
-	    with open(data_log_file,'a') as f:
-	    	f.write(f'{S[1]},{S[2]},{S[3]},{DOP}\n')
+	trigz = swp.extract_triggers(y2)
 
-    # estr = 'warnings: '
-# All kinds of warnings!!!
-    if Nroll < 180:
-        # print('Warning: insufficient points per revolution for accurate data - slow\'er down!')
-        estr+=f'PPC too low ({int(Nroll)})    '
-    if DOP-1 > .03:
-        # print(f'Warning: Possible unphysical DOP = {round(DOP,2)} measured...')
-        estr += f'Unphysical DOP ({round(DOP,3)})    '
-    # if n0 > 4e-2:
-    #     # print(f'Warning: Possible alignment error: large cos(2wt) component detected (S,C) = ({b0/nrm},{n0/nrm})')
-    #     estr += f'non-zero cos(2w): {round(n0,2)} cf {round(b0*prf/nrm,2)}    '
-        
-    if np.mean(y1) < 0.08:
-        estr += f'Light level too low    '
-        
-    if Nchunks < 3:
-        estr += f'Insufficient chunks    '
-      
-    txt_err.set_text(f'({round(np.mean(y1),2)},{round(S[1],2)},{round(S[2],2)},{round(S[3],2)})\n'+estr)
-    x,y = swp.get_polarization_ellipse(S)
-    
-    txt1.set_text(f'DOP: {round(DOP,3)}')
-    if run_offline:
-        urr = 100*abs(DOP - sim_DOP)/sim_DOP
-        txt2.set_text(f'Error: {round(urr,1)}%')
-    else:
-        txt2.set_text(f'Mean Signal: {np.mean(y1)}')
-        
-    ln1.set_data(x,y)
-    
-    for i in range(len(S)):
-        bar[i].set_height(S[i])
-        if run_offline:
-            bar2[i].set_height(S_sim[i]/S_sim[0])
+	# I'm rewriting this part of the algorithm, if only because I'm too stoopid to git it. - AM
+	Nchunks = len(trigz)-1
 
-    
-    ln3.set_data(range(len(y1)),y1)
-    ax3.set_xlim(0, len(y1))
-    # ln3.set_data(range(len(chunk)),chunk)
-    # if auto_scale_y_trace:
-    #     ax3.set_ylim(min(chunk) + 0.001, max(chunk) +0.001)
-    # ax3.set_xlim(0, len(chunk))
-    
-    return ln1, bar, txt1, ln3,
+	Nroll = 0 # Holds rolling average of pts per chunk (PPC). Used for accuracy warning.
+	# For each chunk we calculate the 0,2w, and 4w comonents, averaged over all chunks
+	# get_stokes_from_chunk(cnk,wp_ret = np.pi/2,phs_ofst = 0,verbose = False):
+
+	S = np.zeros(4)
+
+	for k in range(Nchunks):
+		chunk = np.array(y1[trigz[k]:trigz[k+1]])
+		#get_stokes_from_chunk(cnk,wp_ret = np.pi/2,phs_ofst = 0,verbose = False):
+		S += swp.get_stokes_from_chunk(chunk,wp_ret = wp_phi,phs_ofst = trigger_phase,verbose = False)
+		Nroll = (Nroll*k + len(chunk))/(k+1) # Update (PPC)
+		
+	#computing Stokes Parameters from Fourier Shenanigans (see analysis document)
+	S/=Nchunks
+	S /= S[0]
+
+	DOP = np.sqrt(S[1]**2 + S[2]**2 + S[3]**2)
+	if do_save:
+		with open(data_log_file,'a') as f:
+			f.write(f'{S[1]},{S[2]},{S[3]},{DOP}\n')
+
+	# estr = 'warnings: '
+	# All kinds of warnings!!!
+	if Nroll < 180:
+		# print('Warning: insufficient points per revolution for accurate data - slow\'er down!')
+		estr+=f'PPC too low ({int(Nroll)})    '
+	if DOP-1 > .03:
+		# print(f'Warning: Possible unphysical DOP = {round(DOP,2)} measured...')
+		estr += f'Unphysical DOP ({round(DOP,3)})    '
+		
+	# if n0 > 4e-2:
+		## print(f'Warning: Possible alignment error: large cos(2wt) component detected (S,C) = ({b0/nrm},{n0/nrm})')
+		#estr += f'non-zero cos(2w): {round(n0,2)} cf {round(b0*prf/nrm,2)}    '
+
+	if np.mean(y1) < 0.08:
+		estr += f'Light level too low    '
+
+	if Nchunks < 3:
+		estr += f'Insufficient chunks    '
+
+	txt_err.set_text(f'({round(np.mean(y1),2)},{round(S[1],2)},{round(S[2],2)},{round(S[3],2)})\n'+estr)
+	x,y = swp.get_polarization_ellipse(S)
+
+	txt1.set_text(f'DOP: {round(DOP,3)}')
+	if run_offline:
+		urr = 100*abs(DOP - sim_DOP)/sim_DOP
+		txt2.set_text(f'Error: {round(urr,1)}%')
+	else:
+		txt2.set_text(f'Mean Signal: {np.mean(y1)}')
+
+	ln1.set_data(x,y)
+
+	for i in range(len(S)):
+		bar[i].set_height(S[i])
+		if run_offline:
+			bar2[i].set_height(S_sim[i]/S_sim[0])
+
+	ln3.set_data(range(len(y1)),y1)
+	ax3.set_xlim(0, len(y1))
+	# ln3.set_data(range(len(chunk)),chunk)
+	# if auto_scale_y_trace:
+	#     ax3.set_ylim(min(chunk) + 0.001, max(chunk) +0.001)
+	# ax3.set_xlim(0, len(chunk))
+	
+	return ln1, bar, txt1, ln3,
     
 annie = animation.FuncAnimation(fig,animate_fun,init_func = init_animation, interval = 250)
 plt.show()
